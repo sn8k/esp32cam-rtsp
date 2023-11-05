0911ac747a4832dbe93fb7d029c1cf37664fcdb0
YanG Soft <sn8k75@gmail.com>
2023-11-05 20:31:34 +0100

    Update lookup_camera_config.h
    replaced aithinker and esp32 to be compatible with 2 aliexpress board : OV5640 V1.2.1 and ESP32-CAM PSRAM V1.3

--- include/lookup_camera_config.h
+++ include/lookup_camera_config.h
@@ -9,6 +9,7 @@ typedef struct
     const camera_config_t config;
 } camera_config_entry_t;
 
+//esp32cam pour ...esp32-cam PSRAM
 constexpr camera_config_t esp32cam_settings = {
     .pin_pwdn = -1,
     .pin_reset = 15,
@@ -22,11 +23,11 @@ constexpr camera_config_t esp32cam_settings = {
     .pin_d3 = 5,
     .pin_d2 = 34,
     .pin_d1 = 35,
-    .pin_d0 = 17,
+    .pin_d0 = 33,
     .pin_vsync = 22,
     .pin_href = 26,
     .pin_pclk = 21,
-    .xclk_freq_hz = 20000000,
+    .xclk_freq_hz = 10000000,
     .ledc_timer = LEDC_TIMER_0,
     .ledc_channel = LEDC_CHANNEL_0,
     .pixel_format = PIXFORMAT_JPEG,
@@ -34,24 +35,25 @@ constexpr camera_config_t esp32cam_settings = {
     .jpeg_quality = 12,
     .fb_count = 2};
 
+//AITINker pour OV5640 V1.2.1
 constexpr camera_config_t esp32cam_aithinker_settings = {
-    .pin_pwdn = 32,
-    .pin_reset = -1,
-    .pin_xclk = 0,
-    .pin_sscb_sda = 26,
-    .pin_sscb_scl = 27,
-    .pin_d7 = 35,
+    .pin_pwdn = -1,
+    .pin_reset = 5,
+    .pin_xclk = 15,
+    .pin_sscb_sda = 22,
+    .pin_sscb_scl = 23,
+    .pin_d7 = 39,
     .pin_d6 = 34,
-    .pin_d5 = 39,
-    .pin_d4 = 36,
-    .pin_d3 = 21,
-    .pin_d2 = 19,
-    .pin_d1 = 18,
-    .pin_d0 = 5,
-    .pin_vsync = 25,
-    .pin_href = 23,
-    .pin_pclk = 22,
-    .xclk_freq_hz = 20000000,
+    .pin_d5 = 33,
+    .pin_d4 = 27,
+    .pin_d3 = 12,
+    .pin_d2 = 35,
+    .pin_d1 = 14,
+    .pin_d0 = 2,
+    .pin_vsync = 18,
+    .pin_href = 36,
+    .pin_pclk = 26,
+    .xclk_freq_hz = 10000000,
     .ledc_timer = LEDC_TIMER_1,
     .ledc_channel = LEDC_CHANNEL_1,
     .pixel_format = PIXFORMAT_JPEG,
